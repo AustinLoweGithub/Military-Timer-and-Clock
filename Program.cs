0f7bf3a24c8f3f68a2f22df637cf1222f2547cd1
AustinOnshore <81576463+AustinOnshore@users.noreply.github.com>
2021-04-26 12:04:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MilitaryClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.WriteLine("Welcome to Austin's Military Clock/Timer Program.\n\n Enter \"Timer\" or \"Clock\" to choose functionality.");
+
+            string _selection = Console.ReadLine().ToLower();
+
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            if (_selection == "clock")
+            {
+
+                DateTime now = DateTime.Now;
+                 hours = now.Hour;
+                 minutes = now.Minute;
+                 seconds = now.Second;
+            }
+
+
+            while (hours != 24)
+            {
+
+                if (seconds == 60)
+                {
+
+                    minutes++;
+                    seconds = 0;
+
+                }
+
+                if (minutes == 60)
+                {
+                    hours++;
+                    minutes = 0;
+                    seconds = 0;
+
+                }
+
+                string _hours = string.Format("{0:00}", hours);
+
+                string _minutes = string.Format("{0:00}", minutes);
+
+                string _seconds = string.Format("{0:00}", seconds);
+
+                Console.WriteLine(_hours + ":" + _minutes + ":" + _seconds);
+
+                seconds++;
+
+                Thread.Sleep(1000);
+
+            }
+                Console.ReadLine();
+            }
+            }
+        }
+
+
+
+
